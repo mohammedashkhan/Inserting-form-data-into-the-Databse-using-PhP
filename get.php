6a94a0792781cf4affdb2f216295635132dc2c96
M Ashkhan Ahmed <30875725+mohammedashkhan@users.noreply.github.com>
2020-01-02 18:06:04 +0530

    get data on the browser

---
+++ get.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<?php
+
+   echo $_POST['fname']."<br>";
+   echo $_POST['lname']."<br>";
+   echo $_POST['email']."<br>";
+   echo $_POST['mno']."<br>";
+   echo $_POST['pswd']."<br>";
+
+
+
+
+    
+   ?>
+
+</body>
+</html>
